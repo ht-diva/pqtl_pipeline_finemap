fd1e650cf7416d9b1ef02b0e6028b5a686727074
giulia.pontali <giulia.pontali@fht.org>
2024-07-11 10:14:32 +0200

    some modification on minus10logP

--- workflow/scripts/s03_cojo_finemapping.R
+++ workflow/scripts/s03_cojo_finemapping.R
@@ -172,20 +172,26 @@ cat(paste0("done."))
 ## Remove eventually empty dataframes (caused by p_thresh4 filter)
 conditional.dataset$results <- conditional.dataset$results %>% discard(is.null)
 
-# Define the function to compute minuslog10pval and minuslog10pvalC
-compute_pvals <- function(df) {
+# Compute minuslog10pval and minuslog10pvalC
+results_names <- names(conditional.dataset$results)
+
+for (name in results_names) {
+  # Access the dataframe
+  df <- conditional.dataset$results[[name]]
+
+  # Calculate the p-value using the provided formula
   pval <- 2 * pnorm(mpfr(-abs(df$b / df$se), 120))
+  # Calculate the minuslog10pval and add it to the dataframe
   df$minuslog10pval <- as.numeric(-log10(pval))
 
+  # Calculate the p-value using the provided formula
   pvalC <- 2 * pnorm(mpfr(-abs(df$bC / df$bC_se), 120))
+  # Calculate the minuslog10pval and add it to the dataframe
   df$minuslog10pvalC <- as.numeric(-log10(pvalC))
 
-  return(df)
+  # Store the updated dataframe back in the list
+  conditional.dataset$results[[name]] <- df
 }
-
-# Iterate over each dataframe within conditional.dataset$results and apply compute_pvals
-conditional.dataset$results <- conditional.dataset$results %>%
-  map(~ .x %>% compute_pvals())
 
 saveRDS(conditional.dataset, file=paste0(opt$outdir, "/conditional_data_", locus_name, "_up.rds"))
 
