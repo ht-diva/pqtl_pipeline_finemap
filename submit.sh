4bd56f1e3ba655f4558b5b0dc2940fb1f8036452
dariushghasemi <ghasemi.dariush@yahoo.com>
2024-08-21 14:36:31 +0200

    Set parameters for standard cojo

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name locojo
-#SBATCH --output %j_meta_std_cojo.log
+#SBATCH --output %j_meta_std.log
 #SBATCH --partition cpuq
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4G
