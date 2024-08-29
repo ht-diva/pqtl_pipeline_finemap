36c9a4d27acc02eebf5c55ab3720e4e8f3d5eb7a
dariushghasemi <ghasemi.dariush@yahoo.com>
2024-08-29 18:29:35 +0200

    To avoid rule collect loci fails, first scan all, then merge with input seqids #22

--- workflow/scripts/s04_collect_credible_sets.R
+++ workflow/scripts/s04_collect_credible_sets.R
@@ -1,36 +1,52 @@
 #!/usr/bin/Rscript
 
-library(data.table)
-library(dplyr)
+suppressMessages(library(tidyverse))
+suppressMessages(library(data.table))
 
 #----------#
 # taking variants file as input
 sets_path <- snakemake@input
 file_path <- snakemake@output[["ofile"]]
 
+#--------------#
+# the path where COJO+cond SNPs are saved
+exmpl_path <- as.character(sets_path[1])
+pcmd <- dirname(exmpl_path)
 
 #--------------#
-# Load-in COJO results for all proteins sequence
-credible_set_list <- lapply(
+# scan COJO results for all proteins sequence
+cojo_files <- list.files(
+  pattern = paste0("seq.(\\d+).(\\d+)_locus_chr(\\d+)_(\\d+)_(\\d+)_conditional_snps.tsv"),
+  path = pcmd,      # the path where the independents snps file live
+  recursive = FALSE, # to show the files in subdirectories or subfolders
+  full.names = TRUE # to show full path
+)
+
+#--------------#
+# extract seqid from input sentinel files
+input_seqid <- map_dfr(
   sets_path, function(path) {
-    base_path = dirname(path)
-    file_name = basename(path)
-    seqid = stringr::str_remove(file_name, ".sentinel") # extract the protein sequence id and remove file format
-    list.files(
-      pattern = paste0(seqid, "_locus_chr(\\d+)_(\\d+)_(\\d+)_conditional_snps.tsv"), # pattern of output file name containing independents snps
-      path = base_path, # the path where the independents snps file live
-      recursive = TRUE, # to show the files in subdirectories or subfolders
-      full.names = TRUE # to show full path
-      )}) %>% 
-  unlist()
+      base_path = dirname(path)
+      file_name = basename(path)
+      seqid = gsub(".sentinel", "", file_name) # extract the protein sequence id and remove file format
+  data.frame(base_path, file_name, seqid)
+  }
+)
 
 #--------------#
-# Merge them
+# extract seqid from COJO outputs
+seq_list_tbl <- tibble(cojo_files) %>% mutate(seqid = str_extract(cojo_files, "seq.\\d+.\\d+"))
+
+# select input seqids from all present COJO outputs
+cojo_files_input <- cojo_files[seq_list_tbl$seqid %in% input_seqid$seqid]
+
+#--------------#
+# Merge COJO+cond SNPs characteristics
 cojo_meta <- tibble(
   data.table::rbindlist(
     fill = TRUE,
     lapply(
-      credible_set_list, 
+      cojo_files_input, 
       function(x) {
         data.table::fread(x, data.table=F, fill = TRUE) %>% 
         mutate(
@@ -41,9 +57,9 @@ cojo_meta <- tibble(
     }
     )
   )
-) %>%
-  arrange(Chr, bp)
+)
 
+cojo_meta <- cojo_meta %>% arrange(Chr, bp)
 
 #--------------#
 # save the joint results
