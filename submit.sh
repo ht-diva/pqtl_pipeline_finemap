9b9af9ec1c253ff4c6f69093d7bf292db9e1a366
dariushghasemi <ghasemi.dariush@yahoo.com>
2024-11-19 16:58:24 +0100

    expand hole size to 2 Mbp and run only locus breaker; #28

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name locojo
-#SBATCH --output %j_meta_hole_1M.log
+#SBATCH --output %j_meta_hole_2M.log
 #SBATCH --partition cpuq
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4G
