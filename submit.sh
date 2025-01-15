21cd2a26714085c7632bf408e19c2fd94b31a0b7
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-01-15 14:57:21 +0100

    run colocalization analysis on all proteins #29
    setting hole=3M in config

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name locojo
-#SBATCH --output %j_meta_hole_3M.log
+#SBATCH --job-name coloc
+#SBATCH --output %j_meta_coloc.log
 #SBATCH --partition cpuq
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4G
