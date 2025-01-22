111983cebda501d7c713914f2c6b77afbdc45793
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-01-22 12:01:27 +0100

    run pipe on GWAS by batches #32; configurations modified

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name coloc
-#SBATCH --output %j_meta_coloc.log
+#SBATCH --output %j_meta_bybatch.log
 #SBATCH --partition cpuq
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 4G
